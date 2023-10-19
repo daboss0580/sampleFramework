4dd9fedfecdf633f46913b1b40211166148013a2
daboss0580 <iamjustin0580@gmail.com>
2023-10-19 14:17:00 +0100

    SampleFrameworkForJustin

--- src/test/java/APIStepDef/APIWorkFlowStep.java
+++ src/test/java/APIStepDef/APIWorkFlowStep.java
@@ -16,7 +16,7 @@ import java.util.Set;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 
-public class APIWorkFlowSteps {
+public class APIWorkFlowStep {
 
 
 
--- src/test/java/steps/PageInitializer.java
+++ src/test/java/steps/PageInitializer.java
@@ -6,12 +6,10 @@ import pages.*;
 public class PageInitializer {
 
     public static LoginPage loginPage;
-    public static DashboardPage dashboardPage;
 
 
 
    public static void initializePageObjects(){
        loginPage = new LoginPage();
-       dashboardPage = new DashboardPage();
    }
 }
